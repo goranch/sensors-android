18123097301865c48e2d0f7f608f48887bdc6037
Goran CH <cacevski@gmail.com>
2014-04-05 11:09:55 +0300

    Additional sensors added.

--- sensor_android/src/com/example/sensorandroid/SensorActivity.java
+++ sensor_android/src/com/example/sensorandroid/SensorActivity.java
@@ -1,6 +1,8 @@
 package com.example.sensorandroid;
 
-import com.example.sensor_android.R;
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import android.app.Activity;
 import android.hardware.Sensor;
@@ -18,6 +20,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.sensor_android.R;
 
 public class SensorActivity extends Activity implements SensorEventListener,
 		OnClickListener {
@@ -87,7 +91,57 @@ public class SensorActivity extends Activity implements SensorEventListener,
 		mSensorRotationVector = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
 		mSensorTemperature = mSensorManager.getDefaultSensor(Sensor.TYPE_TEMPERATURE);
 	}
+	static int interval;
+	static Timer timer;
 
+	public void timer(){
+		Scanner sc = new Scanner(System.in);
+	    System.out.print("Input seconds => : ");
+	    String secs = sc.nextLine();
+	    int delay = 1000;
+	    int period = 1000;
+	    timer = new Timer();
+	    interval = Integer.parseInt(secs);
+	    System.out.println(secs);
+	    timer.scheduleAtFixedRate(new TimerTask() {
+
+	        public void run() {
+	            
+	        }
+	    }, delay, period);
+	}
+
+	public class ThreadA {
+	    public void main(){
+	        ThreadB b = new ThreadB();
+	        b.start();
+	 
+	        synchronized(b){
+	            try{
+	                System.out.println("Waiting for b to complete...");
+	                b.wait();
+	            }catch(InterruptedException e){
+	                e.printStackTrace();
+	            }
+	 
+	            System.out.println("Total is: " + b.total);
+	        }
+	    }
+	}
+	 
+	class ThreadB extends Thread{
+	    int total;
+	    @Override
+	    public void run(){
+	        synchronized(this){
+	            for(int i=0; i<100 ; i++){
+	                total += i;
+	            }
+	            notify();
+	        }
+	    }
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
